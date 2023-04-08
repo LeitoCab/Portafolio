888892624d3239701b48c54b35ed5483d6d5499b
Leonardo <anthonylrengifo@gmail.com>
2023-04-08 05:41:30 -0500

    Sexto commit: Terminado

--- script.js
+++ script.js
@@ -11,9 +11,30 @@ MenuBtn.addEventListener('click', function(e){
 // Typing Effect
 
 let typed = new Typed('.auto-input',{
-    strings: ['Front-End Developer!', 'Freelancer!', 'UI Designer!', 'Youtuber!'],
+    strings: ['Front-End Developer!', 'Futbolista!', 'UI Designer!', 'Matematico!'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:2000,
     loop: true,
+})
+
+//Active Link State On Scroll
+
+//Get All Links
+let navLinks = document.querySelectorAll('nav ul li a')
+//Get
+let sections = document.querySelectorAll('section')
+console.log(sections)
+window.addEventListener('scroll', function(){
+    const scrollPos= window.scrollY + 20
+    sections.forEach(section=>{
+        if(scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+            navLinks.forEach(link=>{
+                link.classList.remove('active');
+                if(section.getAttribute('id')=== link.getAttribute('href').substring(1)){
+                    link.classList.add('active')
+                }
+            })
+        }
+    })
 })
